81fd854ccaeb331cca19c4afeb516d06d31c4b37
Çağlar GÜNEL <cgunel@gmail.com>
2021-11-16 21:53:00 +0300

    useState

--- react-hooks/src/components/Ingredients/IngredientForm.js
+++ react-hooks/src/components/Ingredients/IngredientForm.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import {useState} from "react";
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
+  const [state, setState] = useState({title:'', amount:''})
+
   const submitHandler = event => {
     event.preventDefault();
     // ...
@@ -15,11 +18,19 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input type="text" id="title" value={state.title}
+                   onChange={event => setState(prevState=> ({
+                       title: event.target.value,
+                       amount: prevState.amount
+                   }))}/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input type="number" id="amount" value={state.amount}
+                   onChange={event => setState(prevState=> ({
+                     amount: event.target.value,
+                     title: prevState.title
+                   }))}/>
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
